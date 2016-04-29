fdb11f00b5b5dd6905697ff6e32bd9a6cb773d7a
Gabry <gabriele.cimato@gmail.com>
2016-04-28 17:04:10 -0700

    fix: indentation and wrong props on Headline

--- scripts/components/homepage-components.jsx
+++ scripts/components/homepage-components.jsx
@@ -8,12 +8,9 @@ import {Headline} from './reusable-components.jsx';
 var BodyButton = React.createClass({
     render: function(){
         return (
-            <Link
-                to="/signup"
-                role="button"
-                className='button-home'
-                >JOIN US
-                </Link>
+            <Link to="/signup" role="button" className='button-home'>
+                JOIN US
+            </Link>
         );
     }
 });
@@ -23,21 +20,22 @@ var SectionIntro = React.createClass({
 		return (
 			<div className='homepage-intro'>
                 <p className="text-center text-grey">
-                FOOD DRIVR
+                    FOOD DRIVR
                 </p>
                 <p className='text-center text-grey'>
-                Powering Donations
+                    Powering Donations
                 </p>
                 <p className='text-center text-grey'>
-                For
+                    For
                 </p>
 				<Headline value="WASTE NOT FOOD TAXI"/>
-				<p className='lead text-center text-grey'>On a daily basis, businesess like catering facilities, restaurants, grocery stores, as well as individuals produce more food than what is necessary for them to meet their needs.
-                <br/>
-                <br/>
-                Organizations that utilize Food Drivr help deliver this excess to people in need.
-                <br/>
-                Join us and help end hunger.
+				<p className='lead text-center text-grey'>
+                    On a daily basis, businesess like catering facilities, restaurants, grocery stores, as well as individuals produce more food than what is necessary for them to meet their needs.
+                    <br/>
+                    <br/>
+                    Organizations that utilize Food Drivr help deliver this excess to people in need.
+                    <br/>
+                    Join us and help end hunger.
 				</p>
                 <br/>
 				<div className='text-center'>
@@ -54,25 +52,25 @@ var SectionHowTo = React.createClass({
             <div className='homepage-howto'>
                 <Headline value="HOW IT WORKS"/>
                 <div className='howtodiv'>
-                <p>DONATING</p>
-                <br/>
-                <p className="text-center">
-                After signing up, donors can enter in items they wish to donate to those in need. Once items are donated, a notification is sent out to all drivers of a pending donation.
-                </p>
+                    <p>DONATING</p>
+                    <br/>
+                    <p className="text-center">
+                        After signing up, donors can enter in items they wish to donate to those in need. Once items are donated, a notification is sent out to all drivers of a pending donation.
+                    </p>
                 </div>
                 <div className='howtodiv'>
-                <p>PICKUP</p>
-                <br/>
-                <p className="text-center">
-                Drivers receive notification of a pending donation and can choose to accept. Upon accepting, they will be given all of the information about the pending donation.
-                </p>
+                    <p>PICKUP</p>
+                    <br/>
+                    <p className="text-center">
+                        Drivers receive notification of a pending donation and can choose to accept. Upon accepting, they will be given all of the information about the pending donation.
+                    </p>
                 </div>
                 <div className='howtodiv'>
-                <p>DELIVERY</p>
-                <br/>
-                <p className="text-center">
-                After the pick up of a donation has been completed, drivers deliver the donation to the nearest pre-determined organization recipient.
-                </p>
+                    <p>DELIVERY</p>
+                    <br/>
+                    <p className="text-center">
+                        After the pick up of a donation has been completed, drivers deliver the donation to the nearest pre-determined organization recipient.
+                    </p>
                 </div>
             </div>
         );
