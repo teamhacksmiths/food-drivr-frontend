97d15f70e26dac5095ae7607500fd0e437f65621
Gabry <gabriele.cimato@gmail.com>
2016-04-29 01:19:31 -0700

    refactor: make comment section responsive

--- scripts/components/homepage-components.jsx
+++ scripts/components/homepage-components.jsx
@@ -110,10 +110,10 @@ var SectionBecomeDriver = React.createClass({
                 <Arrow direction='Left'/>
                 <div className='becomedriver-content text-white'>
                     <Headline value='Become a Driver' className='becomedriver-title'/>
-                    <p>Are people who would rather see their extra food do some good instead of going to waste! We accept donations from business such as catering companies & facilites, restaurants and forcery stores, as well as donations from individuals.</p>
+                    <p>There are people who would rather see their extra food do some good instead of going to waste! We accept donations from business such as catering companies & facilites, restaurants and forcery stores, as well as donations from individuals.</p>
                     <p>As an extra benefit to all of our donors, we provide tax receipts to them so they can write their donations off when they do their taxes.</p>
                     <p>Food Drivr provides an easy way for both drivers and donors to help put an end to hunger in their communities.</p>
-                    <div style={{marginTop: 80}}>
+                    <div className='button-container'>
                         <BodyButton />
                     </div>
                 </div>
