0a2efa8d3479ba2ee3782c8fca5aa5acad06a3f5
Gabry <gabriele.cimato@gmail.com>
2016-05-02 19:07:28 -0700

    Refactor: Add main container background changes

--- scripts/components/main.js
+++ scripts/components/main.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Header, Footer } from './reusable-components.jsx';
+import classNames from 'classnames/bind';
+
+
 
 export default class App extends React.Component {
     render() {
+    	var containerClass = classNames({
+	      container: true,
+	      'blue-background': window.location.pathname === '/',
+	      'red-background': window.location.pathname === '/signup' || window.location.pathname === '/signin',
+	      'lightgrey-background': window.location.pathname === '/donation'
+	    });
         return (
-            <div className='container'>
+            <div className={containerClass}>
             	<Header />
               	{this.props.children}
                 <Footer />
