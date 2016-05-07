91ae1134c5f4e1907fec6d9152d3c986109d812a
Gabry <gabriele.cimato@gmail.com>
2016-05-07 10:20:31 -0700

    Refactor: use functional stateless components

--- scripts/components/signup-components.jsx
+++ scripts/components/signup-components.jsx
@@ -3,66 +3,43 @@ import { Link } from 'react-router';
 import { Headline } from './reusable-components.jsx';
 import Registration from '../containers/registration-container.jsx';
 
+const SignupButton = props => (
+	<div onClick={props.onSubmit} className='btn-signup pointer-cursor'>
+		<img src={props.imageSrc} alt={props.imageAlt}/>
+		<button className='btn-signup' type='submit'>{props.title}</button>
+	</div>
+)
+
 class SignUpPage extends React.Component {
 	constructor(context) {
 		super(context);
-		this.state = {};
-		this.state.userType = '';
-		this.state.userState = false;
-		this.state.class = 'btn-signup';
-		this.state.signupType = '';
-		this.state.style = {
-			background: '#ff1e40'
-		};
-		this.onSubmitDonor = this.onSubmitDonor.bind(this);
-		this.onSubmitVolunteer = this.onSubmitVolunteer.bind(this);
+		this.handleSubmitDonor = this.handleSubmitDonor.bind(this);
+		this.handleSubmitVolunteer = this.handleSubmitVolunteer.bind(this);
 	}
-	onSubmitDonor() {
-		this.setState({ userType: 'Donor',
-						userState: true,
-						class: 'invisible-btn',
-						signupType: 'Donor ',
-						signupType: 'Donor ',
-						style: { background: '#f7b32b' }
-		});
+	handleSubmitDonor() {
 		this.context.router.push('/signup/donor');
 	}
-	onSubmitVolunteer() {
-		this.setState({ userType: 'Volunteer',
-						userState: true,
-						class: 'invisible-btn',
-						signupType: 'Volunteer ',
-						style: { background: '#14cfe8' }
-		});
+	handleSubmitVolunteer() {
 		this.context.router.push('/signup/volunteer');
 	}
 	render() {
 		return (
-			<div className='signup-container text-center text-white' style={this.state.style}>
-			<p>{this.state.signupType + 'Sign Up'}</p>
-			<div onClick={this.onSubmitDonor} className='pointer-cursor' className={this.state.class}>
-				<img src='images/donor-icon.svg' alt='donor package'/>
-				<button
-					className='btn-signup'
-					type='submit'
-					>
-					  Donor
-				</button>
-			</div>
-			<p className={this.state.class}>OR</p>
-			<div onClick={this.onSubmitVolunteer} className='pointer-cursor' className={this.state.class}>
-				<img src='images/volunteer-icon.svg' alt='volounteer truck'/>
-				<button
-					className='btn-signup'
-					type='submit'
-					>
-					  Volunteer
-				</button>
-			</div>
-			{this.state.userState ?
-				<Registration style={this.state.style}/> : null
-			}
-	  </div>
+			<div className='signup-container text-center text-white' style={{background: '#ff1e40'}}>
+				<p>Sign Up</p>
+				<SignupButton
+					onSubmit={this.handleSubmitDonor}
+					imageSrc='images/donor-icon.svg'
+					imgAlt='donor package'
+					title='Donor'
+					/>
+				<p className='btn-signup'>OR</p>
+				<SignupButton
+					onSubmit={this.handleSubmitVolunteer}
+					imageSrc='images/volunteer-icon.svg'
+					imgAlt='volounteer package'
+					title='Donor'
+					/>
+	 		</div>
 		);
 	}
 }
--- scripts/components/user-signup-component.jsx
+++ scripts/components/user-signup-component.jsx
@@ -5,72 +5,64 @@ import TextField from 'material-ui/lib/text-field';
 import RaisedButton from 'material-ui/lib/raised-button';
 import CircularProgress from 'material-ui/lib/circular-progress';
 
-class UserSignup extends React.Component {
-	constructor(props) {
-		super(props);
-		this.displayName = 'UserSignup';
-	}
-	render() {
-		return (
-			<div className='signup-container text-center'>
-				<p>{this.props.userType} Sign Up</p>
-				<form>
-					<TextField
-						hintText="Enter Name"
-						errorText={this.props.errorName}
-						floatingLabelText="Name"
-						onChange={this.props.onNameChange}
-						style={{width: '80%', maxWidth: 350}}
-						errorStyle={{color: 'white'}}
-						value={this.props.name}
-						/>
-						<br/>
-					<TextField
-						hintText="Enter Email"
-						errorText={this.props.errorEmail}
-						floatingLabelText="Email"
-						onChange={this.props.onEmailChange}
-						style={{width: '80%', maxWidth: 350}}
-						errorStyle={{color: 'white'}}
-						value={this.props.email}
-						/>
-						<br/>
-					<TextField
-						hintText="8 or more characters."
-						errorText={this.props.errorPassword}
-						floatingLabelText="Password"
-						onChange={this.props.onPasswordChange}
-						style={{width: '80%', maxWidth: 350}}
-						errorStyle={{color: 'white'}}
-						value={this.props.password}
-						type='password'
-						/>
-						<br/>
-					<TextField
-						hintText="8 or more characters."
-						errorText={this.props.errorPasswordConfirmation}
-						floatingLabelText="Password Confirmation"
-						onChange={this.props.onPasswordConfirmChange}
-						style={{width: '80%', maxWidth: 350}}
-						errorStyle={{color: 'white'}}
-						value={this.props.passwordConfirmation}
-						type='password'
-						/>
-						<br/>
-					<RaisedButton
-						label="Sign Up"
-						secondary={true}
-						onClick={this.props.onSubmitUser}
-						style={{marginTop: 100, minWidth: 250}}
-						/>
-						<br/>
-					<span className="text-lightgrey">
-						{this.props.error}
-					</span>
-				</form>
-			</div>
-		);
-	}
-}
+const UserSignup = props=> (
+	<div className='signup-container text-center'>
+		<p>{props.userType} Sign Up</p>
+		<form>
+			<TextField
+				hintText="Enter Name"
+				errorText={props.errorName}
+				floatingLabelText="Name"
+				onChange={props.onNameChange}
+				style={{width: '80%', maxWidth: 350}}
+				errorStyle={{color: 'white'}}
+				value={props.name}
+				/>
+				<br/>
+			<TextField
+				hintText="Enter Email"
+				errorText={props.errorEmail}
+				floatingLabelText="Email"
+				onChange={props.onEmailChange}
+				style={{width: '80%', maxWidth: 350}}
+				errorStyle={{color: 'white'}}
+				value={props.email}
+				/>
+				<br/>
+			<TextField
+				hintText="8 or more characters."
+				errorText={props.errorPassword}
+				floatingLabelText="Password"
+				onChange={props.onPasswordChange}
+				style={{width: '80%', maxWidth: 350}}
+				errorStyle={{color: 'white'}}
+				value={props.password}
+				type='password'
+				/>
+				<br/>
+			<TextField
+				hintText="8 or more characters."
+				errorText={props.errorPasswordConfirmation}
+				floatingLabelText="Password Confirmation"
+				onChange={props.onPasswordConfirmChange}
+				style={{width: '80%', maxWidth: 350}}
+				errorStyle={{color: 'white'}}
+				value={props.passwordConfirmation}
+				type='password'
+				/>
+				<br/>
+			<RaisedButton
+				label="Sign Up"
+				secondary={true}
+				onClick={props.onSubmitUser}
+				style={{marginTop: 100, minWidth: 250}}
+				/>
+				<br/>
+			<span className="text-lightgrey">
+				{props.error}
+			</span>
+		</form>
+	</div>
+)
 
 export default UserSignup;
