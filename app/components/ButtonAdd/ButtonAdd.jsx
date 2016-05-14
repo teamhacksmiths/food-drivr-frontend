019e541cbe73e12b12ee61e24c79f72af7052a0d
Ryan Collins <rccproductions@mac.com>
2016-05-13 22:51:34 -0400

    Chore: remove legacy components from earlier iteration of address list

--- app/components/ButtonAdd/ButtonAdd.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ButtonAdd = props => (
-  <button
-    className={props.enabled ? 'btn-add-address' : 'btn-add-address btn-disabled'}
-  />
-)
-
-export default ButtonAdd;
