8a7b0b7b88a2f41c9a0fe5768a126862b096bcfc
Gabry <gabriele.cimato@gmail.com>
2016-05-27 17:39:31 -0700

    Feat: add date converter for donation history

--- app/components/DonationPage/DonationHistory.jsx
+++ app/components/DonationPage/DonationHistory.jsx
@@ -3,12 +3,18 @@ import Headline from '../Reusable/Headline';
 import DonationHistoryList from './DonationHistoryList';
 import DonationHistoryItem from './DonationHistoryItem';
 
+const convertDate = function convertDate(date) {
+  const dateItems = date.split(/\-|T/i);
+  const months = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'September', 'October', 'November', 'December'];
+  return `${months[parseInt(dateItems[1] - 1, 10)]} ${dateItems[2]}, ${dateItems[0]}`;
+};
+
 const DonationHistory = ({ donations }) => (
   <section className="donations-history">
     <Headline value="Donations History" />
     <ul className="donations-history__list">
       {donations.map((donation) =>
-        <DonationHistoryList title={donation.participants.donor.name} date={donation.created_at}>
+        <DonationHistoryList title={donation.participants.donor.name} date={convertDate(donation.created_at)}>
           {donation.items.map((item) => <DonationHistoryItem quantity={item.quantity} unit={item.unit} title={item.description} />)}
         </DonationHistoryList>
       )}
