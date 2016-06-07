4f1773e3fd7ffbf5af63a89a10545890e8b409c5
Ryan Collins <rccproductions@mac.com>
2016-06-06 20:37:29 -0400

    Feat: add text as variables in not found.js

--- app/components/Errors/NotFound.jsx
+++ app/components/Errors/NotFound.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
+const oops = () => <b>OOPS,</b>;
+const subHeader = "WE CAN'T SEEM TO FIND THE PAGE YOU'RE LOOKING FOR";
+const paragraphText = "Sorry no page could be found at this address. You can also click on our logo above to be taken back to the Home page.";
 
 const NotFound = () => (
   <div className="container not-found__container">
@@ -9,10 +12,10 @@ const NotFound = () => (
             <div className="not-found__truck"></div>
           </Link>
       </header>
-          <h1 className="not-found__header">OOPS,</h1>
-          <h2 className="not-found__subheader">WE CAN'T SEEM TO FIND THE PAGE YOU'RE LOOKING FOR</h2>
+          <h1 className="not-found__header">{oops}</h1>
+          <h2 className="not-found__subheader">{subHeader}</h2>
           <div className="not-found__paragraph-container">
-            <p className="not-found__paragraph-text">Sorry no page could be found at this address. You can also click on our logo above to be taken back to the Home page.</p>
+            <p className="not-found__paragraph-text">{paragraphText}</p>
           </div>
     </section>
   </div>
