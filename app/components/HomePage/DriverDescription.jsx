2eaaa4cb02b628d926e8f2a9509a0eae016fcfc1
Ryan Collins <rccproductions@mac.com>
2016-06-30 15:58:12 -0400

    Style: donor and driver description refactored to match style guide

--- app/components/HomePage/DriverDescription.jsx
+++ app/components/HomePage/DriverDescription.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const p1 = `There are people who would rather see their extra food do
+  some good instead of going to waste! We accept donations from business such
+  as catering companies & facilites, restaurants and forcery stores,
+  as well as donations from individuals.`;
+
+const p2 = `Food Drivr provides an easy way for both drivers
+  and donors to help put an end to hunger in their communities.`;
+
 const DriverDescription = () => (
   <div>
-    <p>There are people who would rather see their extra food do some good instead of going to waste! We accept donations from business such as catering companies & facilites, restaurants and forcery stores, as well as donations from individuals.</p>
-    <p>Food Drivr provides an easy way for both drivers and donors to help put an end to hunger in their communities.</p>
+    <p>{p1}</p>
+    <p>{p2}</p>
   </div>
 );
 
-module.exports = DriverDescription;
+export default DriverDescription;
