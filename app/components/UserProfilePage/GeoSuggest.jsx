5bf9346851270109b84f8ff764ed72346bcec6f0
Ryan Collins <rccproductions@mac.com>
2016-07-01 20:39:23 -0400

    Feat: add css to match styling for the add button in address list

--- app/components/UserProfilePage/GeoSuggest.jsx
+++ app/components/UserProfilePage/GeoSuggest.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Geosuggest from 'react-geosuggest';
 
 const GeoSuggest = (props) => (
-  <div>
+  <div className="geosuggest__wrapper">
     <label className="geosuggest__label">
-      Address
-      <Geosuggest
-        placeholder="Start typing!"
-        initialValue="Portland"
-        onSuggestSelect={props.onSuggestSelect}
-        location={new google.maps.LatLng(45.523062, -122.676482)}
-        radius="20"
-        disabled={!props.isEditing}
-      />
+      Address List
+      <div className="geosuggest__input-wrapper">
+        <Geosuggest
+          placeholder="Start typing!"
+          initialValue=""
+          onSuggestSelect={props.onSuggestSelect}
+          location={new google.maps.LatLng(45.523062, -122.676482)}
+          radius="20"
+          disabled={!props.isEditing}
+        />
+        <button className="btn button-add"></button>
+      </div>
       <span className="geosuggest__highlight"></span>
       <span className="geosuggest__bar"></span>
     </label>
   </div>
 );
+
+Geosuggest.propTypes = {
+  onSuggestSelect: PropTypes.func.isRequire
+};
 
 export default GeoSuggest;
