1bbf2b24ea42d3f89f14bb3e691e71425f743200
Ryan Collins <rccproductions@mac.com>
2016-08-10 15:19:12 -0400

    Feat: setup express server to serve index.html with bundle build

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-require("babel-core/register");
-var app = require('./server/app');
+/* eslint-disable */
+const webpack = require('webpack');
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpackHotMiddleware = require('webpack-hot-middleware');
+// const config = require('../webpack.config.js');
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 1337 : process.env.PORT;
+const path = require('path');
+// const compiler = webpack(config);
+const express = require('express');
+
+const app = express();
+
+app.use('/build', express.static(__dirname + 'build'));
+
+/* Send the bundle file from the build folder */
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`)
+});
+/* eslint-enable */
