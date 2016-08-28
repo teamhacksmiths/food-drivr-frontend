83bd112c2ad6cee7ffb0f5505f1c030a08d40329
Ryan Collins <rccproductions@mac.com>
2016-08-27 22:40:09 -0400

    Fix: add babel to package.json

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from './app/config/routes';
+// import React from 'react';
+// import { renderToString } from 'react-dom/server'
+// import { match, RouterContext } from 'react-router'
+//import routes from './app/config/routes';
 const app = express();
+const path = require('path');
 
-app.use((req, res) => {
-  // Note that req.url here should be the full URL path from
-  // the original request, including the query string.
-  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-    } else if (renderProps) {
-      // You can also check renderProps.components or renderProps.routes for
-      // your "not found" component or route respectively, and send a 404 as
-      // below, if you're using a catch-all route.
-      res.status(200).send(renderToString(<RouterContext {...renderProps} />));
-    } else {
-      res.status(404).send('Not found');
-    }
-  });
+// app.use((req, res) => {
+//   // Note that req.url here should be the full URL path from
+//   // the original request, including the query string.
+//   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+//     if (error) {
+//       res.status(500).send(error.message);
+//     } else if (redirectLocation) {
+//       res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+//     } else if (renderProps) {
+//       // You can also check renderProps.components or renderProps.routes for
+//       // your "not found" component or route respectively, and send a 404 as
+//       // below, if you're using a catch-all route.
+//       res.status(200).send(renderToString(<RouterContext {...renderProps} />));
+//     } else {
+//       res.status(404).send('Not found');
+//     }
+//   });
+// });
+
+app.use(express.static('./build'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 app.listen(3000);
